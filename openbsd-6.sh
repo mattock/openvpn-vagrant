d4fbafc9164e55f36bd1a59c7a69972249bd8a17
Gert Doering <gert@greenie.muc.de>
2017-11-11 18:38:12 +0100

    Add OpenBSD 6 provisioning script
    This installs the required packages, sets up necessary environment
    variables for autotool versions, and adds convenience links for lzo2
    and mbedtls (otherwise our configure is not finding it, needing LZO2_...
    configure args)
    
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- openbsd-6.sh
+++ openbsd-6.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
 echo "Preparing OpenVPN build environment"
+pkg_add -I sudo-- git autoconf-2.69p2 automake-1.15.1 \
+		fping lzo2 lz4 libtool mbedtls cmake
+
+# OpenBSD wants to be told which auto* version to run (can have multiple)
+echo "setting up AUTO_* environment variables (/etc/profile)"
+echo "export AUTOCONF_VERSION=2.69" >>/etc/profile
+echo "export AUTOMAKE_VERSION=1.15" >>/etc/profile
+
+# OpenBSD puts lzo into /usr/local/{include,lib} where our configure 
+# will not find it -> make symlinks
+echo "creating lzo2 convenience symlinks..."
+cd /usr/include && ln -s ../local/include/lzo .
+cd /usr/lib && ln -s ../local/lib/liblzo2.* .
+
+# same thing for mbedtls...
+echo "creating mbedTLS convenience symlinks..."
+cd /usr/include && ln -s ../local/include/mbedtls .
+cd /usr/lib && ln -s ../local/lib/libmbed* .
+
+echo "done!"
+exit 0
