4f6b5bceae4161e191bb11b9e4882f0b4cc62642
Gert Doering <gert@greenie.muc.de>
2017-11-12 11:35:35 +0100

    NetBSD-7: add missing fping6 symlink
    Signed-off-by: Gert Doering <gert@greenie.muc.de>

--- netbsd-7.sh
+++ netbsd-7.sh
@@ -11,6 +11,10 @@ export PATH PKG_PATH
 # "-U" is needed to get cmake (because libarchive needs to be updated)
 pkg_add -U automake autoconf libtool fping git lzo lz4 mbedtls cmake
 
+# the fping package is stupid and is missing the "fping6" symlink
+echo "fixing missing fping6 symlink..."
+cd /usr/pkg/sbin && ln -s fping fping6
+
 # NetBSD puts lzo into /usr/pkg/{include,lib} where our configure
 # will not find it -> make symlinks
 echo "creating lzo2 convenience symlinks..."
