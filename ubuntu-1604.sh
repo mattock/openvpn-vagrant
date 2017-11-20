10fc4b5147aaf5c515cf5005502f8eb8f6e5757c
Samuli Seppänen <samuli@openvpn.net>
2017-11-20 15:03:17 +0200

    Remove fping from package list on Ubuntu 16.04
    It is not available in the default Ubuntu repositories
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- ubuntu-1604.sh
+++ ubuntu-1604.sh
@@ -2,4 +2,4 @@
 
 echo "Preparing OpenVPN build environment"
 echo
-apt-get -y install liblzo2-dev libssl-dev libpam-dev libpkcs11-helper1-dev libtool autoconf make cmake git fping net-tools liblz4-dev
+apt-get -y install liblzo2-dev libssl-dev libpam-dev libpkcs11-helper1-dev libtool autoconf make cmake git net-tools liblz4-dev
