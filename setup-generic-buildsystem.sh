ad59cd8280f50373b5d2bf4dc4f510e456b160e6
Samuli Seppänen <samuli@openvpn.net>
2020-08-13 14:21:00 +0300

    openvpn-build-bionic: install python3-docutils during provisioning
    This is needed to generate the man-pages
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- setup-generic-buildsystem.sh
+++ setup-generic-buildsystem.sh
@@ -5,7 +5,7 @@
 # This script works on Ubuntu 16.04 and 18.04.
 
 CODENAME=`lsb_release -cs`
-BUILD_DEPS="libpam0g-dev mingw-w64 man2html dos2unix nsis unzip wget curl autoconf libtool gcc-arm-linux-gnueabi"
+BUILD_DEPS="libpam0g-dev mingw-w64 man2html dos2unix nsis unzip wget curl autoconf libtool gcc-arm-linux-gnueabi python3-docutils"
 OSSLSIGNCODE_URL="https://github.com/mtrojnar/osslsigncode.git"
 OPENVPN_BUILD_URL="https://github.com/OpenVPN/openvpn-build.git"
 MINGW_PACKAGES="mingw-w64 mingw-w64-common mingw-w64-i686-dev mingw-w64-x86-64-dev"
