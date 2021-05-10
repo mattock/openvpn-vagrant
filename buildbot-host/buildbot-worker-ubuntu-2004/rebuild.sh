380147b8d807476c454d1eec516f88ae2d5fbd88
Samuli Seppänen <samuli@openvpn.net>
2021-05-10 08:03:05 +0300

    Temporary commit
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- buildbot-host/buildbot-worker-ubuntu-2004/rebuild.sh
+++ buildbot-host/buildbot-worker-ubuntu-2004/rebuild.sh
@@ -5,10 +5,7 @@ BASEDIR=/vagrant/buildbot-host
 # Get configuration parameters
 . $BASEDIR/params
 
-rm -rf scripts
-cp -r ../scripts .
+#rsync -va ../scripts .
 
 docker image rm openvpn_community/buildbot-worker-ubuntu-2004:$WORKER_UBUNTU_2004_IMAGE_TAG
 docker build -f Dockerfile -t openvpn_community/buildbot-worker-ubuntu-2004:$WORKER_UBUNTU_2004_IMAGE_TAG .
-
-rm -rf scripts
---
+++ buildbot-host/buildbot-worker-ubuntu-2004/scripts/install-openvpn-build-deps-ubuntu.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+export DEBIAN_FRONTEND=noninteractive
+
+# Install dependencies
+apt-get update
+apt-get install -y -q --no-install-recommends \
+autoconf \
+automake \
+build-essential \
+cmake \
+git \
+libcmocka-dev \
+libcmocka0 \
+liblz4-dev \
+liblzo2-dev \
+libpam-dev \
+libpkcs11-helper-dev \
+libssl-dev \
+libtool \
+make
