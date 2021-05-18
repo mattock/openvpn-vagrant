7065c0539ebc804fc64f834a5ba12e0b1db7ba83
Samuli Seppänen <samuli@openvpn.net>
2021-05-18 16:23:46 +0000

    Add OpenSuSE Leap 15 worker
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ buildbot-host/scripts/install-openvpn-build-deps-opensuse-leap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+zypper install -y \
+autoconf \
+cmake \
+curl \
+gcc \
+git \
+gnutls-devel \
+gzip \
+libtool \
+liblz4-devel \
+lzo-devel \
+make \
+openssl-devel \
+pam-devel \
+pkcs11-helper-devel \
+python3-devel \
+python3-pip \
+python3-setuptools \
+python3-wheel
