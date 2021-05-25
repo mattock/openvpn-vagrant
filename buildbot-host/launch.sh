7bebcc043fe0f60e00796dbb52fc1fb631d2d5e1
Samuli Seppänen <samuli@openvpn.net>
2021-05-25 07:09:05 +0000

    Make launch.sh t_client.sh-capable
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- buildbot-host/launch.sh
+++ buildbot-host/launch.sh
@@ -22,4 +22,4 @@ TAG=`grep MY_VERSION $DIR/Dockerfile.base|awk 'BEGIN { FS = "\"" }; { print $2 }
 
 docker container stop $IMAGE
 docker container rm $IMAGE
-docker container run -it --name $IMAGE --network buildbot-net --env-file=$DIR/env openvpn_community/$IMAGE:$TAG
+docker container run -it --name $IMAGE --network buildbot-net --cap-add NET_ADMIN --sysctl net.ipv6.conf.all.disable_ipv6=0 --env-file=$DIR/env openvpn_community/$IMAGE:$TAG
