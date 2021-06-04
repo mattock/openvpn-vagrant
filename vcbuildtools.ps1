6d9a3d1c19de8bf3d8a788c91bdac1f2a16e2a89
Samuli Seppänen <samuli@openvpn.net>
2021-06-04 14:58:12 +0300

    Reboot after Visual Studio build tools installation
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vcbuildtools.ps1
+++ vcbuildtools.ps1
@@ -2,3 +2,6 @@ Write-Host "Installing Visual Studio build tools"
 & choco.exe install -y git --params "/GitAndUnixToolsOnPath"
 & choco.exe install -y visualstudio2019buildtools
 & choco.exe install -y visualstudio2019-workload-vctools
+
+Write-Host "Rebooting to finish Visual Studio 2019 build tools installation"
+shutdown /r /t 10
