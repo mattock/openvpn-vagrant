10f3297da1a23d846e3e12e621c601ee7a69a329
Samuli Seppänen <samuli@openvpn.net>
2021-06-04 09:35:47 +0300

    Use standard buildbot.tac even on Windows
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- buildbot.ps1
+++ buildbot.ps1
@@ -5,5 +5,4 @@ $workdir = "C:\Users\vagrant\buildbot"
 & pip.exe --no-cache-dir install twisted[tls]
 & pip.exe --no-cache-dir install buildbot_worker==3.1.0
 New-item -Type directory $workdir
-Copy-Item "${vboxfs}\buildbot-host\buildbot-worker-windows-server-2019\buildbot.tac" $workdir
-#& twistd.exe --nodaemon --python=buildbot.tac 
+Copy-Item "${vboxfs}\buildbot-host\buildbot.tac" $workdir
