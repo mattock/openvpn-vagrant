088c40a77bd14b0f17f65c086113781b8b883f87
Samuli Seppänen <samuli@openvpn.net>
2021-06-08 12:18:44 +0300

    Bake in idempotency to Vagrant provisioning
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vcbuildtools.ps1
+++ vcbuildtools.ps1
@@ -1,7 +1,7 @@
 Write-Host "Installing Visual Studio build tools"
 & choco.exe install -y git --params "/GitAndUnixToolsOnPath"
-& choco.exe install -y visualstudio2019buildtools
-& choco.exe install -y visualstudio2019-workload-vctools
+& choco.exe install -y visualstudio2019buildtools --params "--installPath C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.UWP.VC.ARM64 --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 --quiet"
+#& choco.exe install -y visualstudio2019-workload-vctools
 
 Write-Host "Rebooting to finish Visual Studio 2019 build tools installation"
-shutdown /r /t 10
+shutdown /r /t 30
--- vcpkg.ps1
+++ vcpkg.ps1
@@ -1,11 +1,26 @@
 Write-Host "Setting up openvpn build dependencies with vcpkg"
 $workdir = "C:\Users\vagrant\opt"
 
-New-Item -Type directory $workdir
+if (-Not (Test-Path $workdir)) {
+  New-Item -Type directory $workdir
+}
 
-& git.exe clone -b dockertest https://github.com/mattock/openvpn.git "${workdir}\openvpn"
-& git.exe clone https://github.com/microsoft/vcpkg.git "${workdir}\vcpkg"
+if (-Not (Test-Path "${workdir}\openvpn")) {
+  & git.exe clone -b dockertest https://github.com/mattock/openvpn.git "${workdir}\openvpn"
+}
 
+if (-Not (Test-Path "${workdir}\vcpkg")) {
+  & git.exe clone https://github.com/microsoft/vcpkg.git "${workdir}\vcpkg"
+}
+
+# Bootstrap vcpkg
 & "${workdir}\vcpkg\bootstrap-vcpkg.bat"
-& "${workdir}\vcpkg\vcpkg.exe" --overlay-ports="${workdir}\openvpn\contrib\vcpkg-ports" --overlay-triplets="${workdir}\openvpn\contrib\vcpkg-triplets" install lz4:x64-windows-ovpn lzo:x64-windows-ovpn openssl-windows:x64-windows-ovpn pkcs11-helper:x64-windows-ovpn tap-windows6:x64-windows-ovpn
+
+# Install OpenVPN build dependencies
+$architectures = @('x64','x86','arm64')
+foreach ($arch in $architectures) {
+    & "${workdir}\vcpkg\vcpkg.exe" --overlay-ports="${workdir}\openvpn\contrib\vcpkg-ports" --overlay-triplets="${workdir}\openvpn\contrib\vcpkg-triplets" install "lz4:${arch}-windows-ovpn" "lzo:${arch}-windows-ovpn" "openssl-windows:${arch}-windows-ovpn" "pkcs11-helper:${arch}-windows-ovpn" "tap-windows6:${arch}-windows-ovpn"
+}
+
+# Ensure that OpenVPN build can find the dependencies
 & "${workdir}\vcpkg\vcpkg.exe" integrate install
