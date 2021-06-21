c9c4a8d4f81ebb1b7fce4c9cadf2580857764a2a
Samuli Seppänen <samuli@openvpn.net>
2021-06-21 23:55:41 +0000

    Add debugging for ARM64 installation
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vcbuildtools.ps1
+++ vcbuildtools.ps1
@@ -2,7 +2,11 @@ Write-Host "Installing Visual Studio build tools"
 & choco.exe install -y git --params "/GitAndUnixToolsOnPath"
 & choco.exe install -y visualstudio2019buildtools
 & choco.exe install -y visualstudio2019-workload-vctools
+
+
+Write-Host "Setting up ARM64 support"
 & "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools" --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.UWP.VC.ARM64 --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 --quiet
+Write-Host "Done"
 
 #& choco.exe install -y visualstudio2019buildtools --params "--installPath C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.UWP.VC.ARM64 --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 --quiet"
 
