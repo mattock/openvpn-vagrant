20a48b05d41c2ef531a70496f8d86c941e4beb5a
Samuli Seppänen <samuli@openvpn.net>
2021-06-23 19:59:09 +0300

    Fix Visual Studio ARM64 build tools provisioning
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vsarm64.ps1
+++
@@ -1,2 +0,0 @@
-Write-Host "Setting up ARM64 support for Visual Studio"
-& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools" --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.UWP.VC.ARM64 --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 --quiet
