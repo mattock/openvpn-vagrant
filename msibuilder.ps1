f2e1715a66a5bd357c1f22d57e751cb16f3d195a
Samuli Seppänen <samuli@openvpn.net>
2021-08-03 11:56:58 +0300

    Install WiX 3.14 to enable ARM64 building
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- msibuilder.ps1
+++ msibuilder.ps1
@@ -1,3 +1,11 @@
 Write-Host "Installing dependencies for building MSI packages"
+
+$workdir = "C:\Users\vagrant\Downloads"
+
 & choco.exe install -y git --params "/GitAndUnixToolsOnPath"
-& choco.exe install -y wixtoolset
+
+# Chocolatey package bundles WiX version too old for building ARM64 MSIs.
+#& choco.exe install -y wixtoolset
+
+Invoke-WebRequest -Uri https://wixtoolset.org/downloads/v3.14.0.4118/wix314.exe -Outfile "${workdir}/wix314.exe"
+& "${workdir}/wix314.exe" /q
