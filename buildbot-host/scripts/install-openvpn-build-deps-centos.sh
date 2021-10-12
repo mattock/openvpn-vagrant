c7d14b75299e514d8792622d803c1f0d9e92e2a7
Samuli Seppänen <samuli@openvpn.net>
2021-10-12 12:53:37 +0300

    buildmaster: build-test openvpn3
    This involves new build steps as well installation of asio during docker image
    builds.
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- buildbot-host/scripts/install-openvpn-build-deps-centos.sh
+++ buildbot-host/scripts/install-openvpn-build-deps-centos.sh
@@ -5,7 +5,7 @@ yum -y install epel-release
 
 yum -y install \
 autoconf \
-cmake \
+cmake3 \
 gcc \
 gcc-c++ \
 git \
@@ -21,3 +21,7 @@ python3-devel \
 python3-pip \
 python3-setuptools \
 python3-wheel
+
+# This is required for CentoOS 7 where "cmake" is missing, but "cmake3" is
+# present.
+test -e /usr/bin/cmake || ln -s /usr/bin/cmake3 /usr/bin/cmake
