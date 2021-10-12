8fc920de5558d49953047422581dff7eee626164
Samuli Seppänen <samuli@openvpn.net>
2021-10-12 12:54:40 +0300

    buildmaster: add arch buildbot worker
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ buildbot-host/scripts/install-openvpn-build-deps-arch.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Upgrade package cache and do a system upgrade
+pacman -Syu
+
+# Install build dependencies
+pacman -S --noconfirm \
+autoconf \
+automake \
+cmake \
+curl \
+gcc \
+git \
+gnutls \
+libtool \
+lz4 \
+lzo \
+make \
+openssl \
+pam \
+pkcs11-helper \
+python \
+python-pip \
+python-setuptools \
+python-wheel
