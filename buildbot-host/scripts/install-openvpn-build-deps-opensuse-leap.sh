315cf9c70dcc2bc4a1cb5aa203984d7cb7c04ade
Samuli Seppänen <samuli@openvpn.net>
2021-11-04 14:07:08 +0100

    buildbot: make OpenSuSE 15 able to build openvpn3-linux
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- buildbot-host/scripts/install-openvpn-build-deps-opensuse-leap.sh
+++ buildbot-host/scripts/install-openvpn-build-deps-opensuse-leap.sh
@@ -4,21 +4,38 @@ set -ex
 
 zypper install -y \
 autoconf \
+autoconf-archive \
+automake \
+bzip2 \
 cmake \
 curl \
 gcc \
 gcc-c++ \
 git \
+glib2-devel \
 gnutls-devel \
 gzip \
-libtool \
+jsoncpp-devel \
+libcap-ng-devel \
 liblz4-devel \
+libxml2-devel \
+libtool \
+libselinux-devel \
+libuuid-devel \
 lzo-devel \
 make \
 openssl-devel \
 pam-devel \
 pkcs11-helper-devel \
+pkgconf \
+polkit \
 python3-devel \
+python3-dbus-python \
+python3-docutils \
+python3-gobject \
+python3-Jinja2 \
 python3-pip \
+python3-pyOpenSSL \
 python3-setuptools \
-python3-wheel
+python3-wheel \
+tinyxml2-devel
