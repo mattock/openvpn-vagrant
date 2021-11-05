c7f28c06527186414ec2030c29c5c09debfab1dd
Samuli Seppänen <samuli@openvpn.net>
2021-11-05 16:41:17 +0100

    buildbot: make removal of intermediary images explicit
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- buildbot-host/rebuild.sh
+++ buildbot-host/rebuild.sh
@@ -41,7 +41,7 @@ IMAGE=`grep MY_NAME $DIR/Dockerfile|awk 'BEGIN { FS = "\"" }; { print $2 }'`
 TAG=`grep MY_VERSION $DIR/Dockerfile|awk 'BEGIN { FS = "\"" }; { print $2 }'`
 
 # Remove image with same name and tag, if found
-docker build -f $DIR/Dockerfile -t openvpn_community/$IMAGE:$TAG .
+docker build -f $DIR/Dockerfile --rm=true -t openvpn_community/$IMAGE:$TAG .
 
 if [ "${DYNAMIC_DOCKERFILE}" = "yes" ]; then
   rm -f $DIR/Dockerfile
